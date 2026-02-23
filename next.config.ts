a86913fc4e988b236892a291bc49f45eb9b06492
wdelong57 <wdelong007@gmail.com>
2026-02-23 15:20:46 +0000

    Fix next.config.ts duplicate nextConfig

--- next.config.ts
+++ next.config.ts
@@ -13,10 +13,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
